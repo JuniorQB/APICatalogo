cb368b7114cf7454122e6b91a07c012d7b9eb488
Florivaldo Junior <eucomcerteza@gmail.com>
2024-02-21 19:40:46 -0300

    adjusting the migrations

--- APICatalogo/Models/Category.cs
+++ APICatalogo/Models/Category.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Categories")]
 public class Category
 {
+    [Key]
     public int CategoryId { get; set; }
+    [Required]
+    [StringLength(100)]
     public string? Name { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? ImageURL { get; set; }
 
     public ICollection<Product> Products { get; set; }
