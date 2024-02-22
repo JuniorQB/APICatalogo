5c1f5ee01c66f223b92e5de04cae1e380cf27ca6
Florivaldo Junior <eucomcerteza@gmail.com>
2024-02-22 12:19:50 -0300

    Optimizing serialization JSon results

--- APICatalogo/Models/Product.cs
+++ APICatalogo/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 
@@ -26,5 +27,6 @@ public class Product
 
     public int CategoryId { get; set; }
 
+    [JsonIgnore]
     public Category? Category { get; set; }
 }
