fcccbff787c953dd697cedc5b14a0f7735c7a933
Florivaldo Junior <eucomcerteza@gmail.com>
2024-02-22 11:08:33 -0300

    Created controller products

---
+++ APICatalogo/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+
+    private readonly AppDbContext _context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+}
