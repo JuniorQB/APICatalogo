ab210789e656ac67dbea24e9d693d6644afc6b80
Florivaldo Junior <eucomcerteza@gmail.com>
2024-02-23 18:05:47 -0300

    Created FilterException

---
+++ APICatalogo/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace APICatalogo.Filters;
+
+public class ApiExceptionFilter : IExceptionFilter
+{
+
+    private readonly ILogger<ApiExceptionFilter> logger;
+    public ApiExceptionFilter(ILogger<ApiExceptionFilter> logger)
+    {
+
+        this.logger = logger;
+
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        logger.LogError(context.Exception, "Ocorreu uma exceção não tratada: Status code 500");
+        context.Result = new ObjectResult("\"Ocorreu uma exceção não tratada: Status code 500")
+        {
+            StatusCode = StatusCodes.Status500InternalServerError,
+        };
+
+    }
+}
