148fdf6dff9a92770620eec3868b5b2957c95fa0
Florivaldo Junior <eucomcerteza@gmail.com>
2024-02-23 16:46:50 -0300

    Implemented logger

---
+++ APICatalogo/Logging/CustomLogger.cs
@@ -0,0 +1,48 @@
+﻿
+namespace APICatalogo.Logging;
+
+public class CustomLogger : ILogger
+{
+    readonly string loggerName;
+    readonly CustomLoggerProviderConfiguration loggerConfig;
+
+    public CustomLogger(string loggerName, CustomLoggerProviderConfiguration loggerConfig)
+    {
+        this.loggerName = loggerName;
+        this.loggerConfig = loggerConfig;
+    }
+
+    public IDisposable BeginScope<TState>(TState state) 
+    {
+        return null;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel == loggerConfig.LogLevel;
+    }
+
+   public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, 
+       Exception exception, Func<TState, Exception, string> formatter)
+    {
+        string message = $"{logLevel.ToString()}: {eventId.Id} - {formatter(state, exception)}";
+
+        WriteTextInFile(message);
+    }
+
+    private void WriteTextInFile(string message)
+    {
+        string filePath = @"C:\temp\ApiLog.txt";
+
+        using (StreamWriter sw = new StreamWriter(filePath, true)) { 
+            try
+            {
+                sw.WriteLine(message);
+                sw.Close();
+            }catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ APICatalogo/Logging/CustomLoggerProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+namespace APICatalogo.Logging;
+
+public class CustomLoggerProvider : ILoggerProvider
+{
+    readonly CustomLoggerProviderConfiguration loggerConfig;
+
+    readonly ConcurrentDictionary<string, CustomLogger> logger = new ConcurrentDictionary<string, CustomLogger>();
+
+    public CustomLoggerProvider(CustomLoggerProviderConfiguration loggerConfig)
+    {
+        this.loggerConfig = loggerConfig;
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return logger.GetOrAdd(categoryName, name => new CustomLogger(name, loggerConfig));
+    }
+
+    public void Dispose()
+    {
+        logger.Clear();
+    }
+}
