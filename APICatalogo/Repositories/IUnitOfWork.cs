63e3637cff6547ce976f117ea47d8e7968735959
Florivaldo Junior <eucomcerteza@gmail.com>
2024-02-27 20:16:48 -0300

    Created class and Interface UnityOfWork

---
+++ APICatalogo/Repositories/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace APICatalogo.Repositories;
+
+public interface IUnitOfWork
+{
+    IProductRepository ProductRepository { get; }
+    ICategorieRepository CategorieRepository { get; }
+
+    void Commit();
+    
+}
---
+++ APICatalogo/Repositories/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using APICatalogo.Context;
+
+namespace APICatalogo.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+
+{
+    private IProductRepository? _productRepository;
+
+    private ICategorieRepository? _categorieRepository;
+
+    public AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public ICategorieRepository CategorieRepository
+    {
+        get
+        {
+            return _categorieRepository = _categorieRepository ?? new CategoryRepository(_context);
+        }
+    }
+
+    public IProductRepository ProductRepository 
+    {
+        get {
+            return _productRepository = _productRepository ?? new ProductRepository(_context);
+        }
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
