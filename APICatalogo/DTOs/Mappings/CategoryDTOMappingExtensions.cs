0bccb9953d1ee368039ae361d38cf0b4e82b2c61
Florivaldo Junior <eucomcerteza@gmail.com>
2024-02-28 11:37:38 -0300

    Implemented Extension Method DTO - CategoryDTO

---
+++ APICatalogo/DTOs/Mappings/CategoryDTOMappingExtensions.cs
@@ -0,0 +1,48 @@
+﻿using APICatalogo.Models;
+using System.Xml.Linq;
+
+namespace APICatalogo.DTOs.Mappings;
+
+public static class CategoryDTOMappingExtensions
+{
+    public static CategoryDTO? ToCategoryDTO(this CategoryDTO category)
+    {
+        if (category is null)
+        {
+            return null;
+        }
+        return new CategoryDTO()
+        {
+            CategoryId = category.CategoryId,
+            Name = category.Name,
+            ImageURL = category.ImageURL
+        };
+    }
+
+    public static Category? ToCategory(this CategoryDTO categoryDTO)
+    {
+        if (categoryDTO is null) { return null;  }
+
+        return new Category()
+        {
+            CategoryId = categoryDTO.CategoryId,
+            Name = categoryDTO.Name,
+            ImageURL = categoryDTO.ImageURL
+        };
+    }
+
+    public static IEnumerable<CategoryDTO> ToCategoryDtoList(this IEnumerable<Category> categories)
+    {
+        if (categories is null || !categories.Any()) { return new List<CategoryDTO>(); }
+
+        return categories.Select(category => new CategoryDTO()
+        {
+            CategoryId = category.CategoryId,
+            Name = category.Name,
+            ImageURL = category.ImageURL,
+        }).ToList();
+
+
+    
+    }
+}
